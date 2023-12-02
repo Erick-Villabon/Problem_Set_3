590494f7f6a9b983e65d5b876715c4762ae0142e
diegodduartebb <diegoduartebello@gmail.com>
2023-12-02 17:33:36 -0500

    Clasificacion

--- scripts/2_Pronosticos_Clasificacion.R
+++ scripts/2_Pronosticos_Clasificacion.R
@@ -14,7 +14,13 @@ library(pacman)
 p_load(tidyverse, # Manipular dataframe
        tidymodels, # ML modelos
        yardstick, # Matriz de Confusión
-       ggplot2) # Graficas
+       ggplot2,
+       caret,
+       rpart,
+       ranger,
+       tune, rsample, parsnip,
+       units, randomForest, rattle, xgboost, bst, caret, keras, discrim, plyr, dplyr,
+       openxlsx, adabag) 
 
 # - Revisar el espacio de trabajo
 setwd("/Users/juandiego/Desktop/GitHub/Problem_Set_3/stores")
@@ -28,101 +34,130 @@ test <- read.xlsx("test_2.xlsx")
 
 train <- read.xlsx("train_2.xlsx")
 
-submission_template <- read.csv("submission_template.csv")
+submission_template <- read.csv("sample_submission.csv")
 
 
 
 ##Preparar las bases 
 
 train$Ingtot <- with(train, ifelse(is.na(Ingtot),Ingtotug,Ingtot))
+
+
+##Defino
+pobre<-train$Pobre 
+pobre_1<-test$Pobre 
+
+#test<-test %>% mutate(Pobre=factor(Pobre,levels=c(0,1),labels=c("No","Si")))
+#train<-train %>% mutate(Pobre=factor(Pobre,levels=c(0,1),labels=c("No","Si")))
+
+
 db <- rbind(test, train)
 
 
-#Dividir los datos en train y test
+rec_1 <- recipe(Pobre ~ edad + edad_2 + mujer + estudiante + 
+                  primaria + secundaria  + media + 
+                  superior + exp_trab_actual + cuartosxpersonas + 
+                  num_menores + ciudad + amortizacion + arriendo1 + casapropia +
+                  casahipoteca + casausufructo + casasintitulo + casaarriendo + Des +
+                  Ina, data = db) %>%
+  step_novel(all_nominal_predictors()) %>% 
+  step_dummy(all_nominal_predictors()) %>% 
+  step_zv(all_predictors()) 
+
+
+
+
+ctrl<- trainControl(method = "cv",
+                    number = 5,
+                    classProbs = TRUE,
+                    verbose=FALSE,
+                    savePredictions = T)
+
+#LOGIT
+##########
+
 set.seed(123)
-split <- initial_split(train, prop = .75)
-train_data <- training(split)
-test_data  <- testing(split)
+mylogit_caret <- train(rec_1, 
+                       data = train, 
+                       method = "glm",
+                       trControl = ctrl,
+                       family = "binomial")
 
-#Crear un recipe
-receta <- recipe( Ingtot ~ edad + edad_2 + mujer + estudiante + 
-                    primaria + secundaria  + media + 
-                    superior + exp_trab_actual + cuartosxpersonas + 
-                    num_menores + ciudad + amortizacion + arriendo1 + casapropia +
-                    casahipoteca + casausufructo + casasintitulo + casaarriendo + Des +
-                    Ina, data = train_data) %>% 
-  step_rm(in_sf) %>% # eliminamos la variable continua que tiene la misma info que la variable objetivo
-  step_center(all_predictors()) %>% # Centramos todas las variables
-  step_scale(all_predictors()) # Reescalamos
+mylogit_caret
 
+logit_pred <- predict(mylogit_caret, newdata = test) %>% 
+  bind_cols(test) 
 
+logit_pred<- logit_pred %>%
+  select(id, ...1)
 
-receta_lineal <- recipe(Ingtot ~ edad + edad_2 + mujer + estudiante + 
-                          primaria + secundaria  + media + 
-                          superior + exp_trab_actual + cuartosxpersonas + 
-                          num_menores + ciudad + amortizacion + arriendo1 + casapropia +
-                          casahipoteca + casausufructo + casasintitulo + casaarriendo + Des +
-                          Ina, data = train_data) %>%
-  step_rm(ciudad) %>% # eliminamos la variable categórica que tiene la misma info que la variable objetivo
-  step_center(all_predictors()) %>% # Centramos todas las variables
-  step_scale(all_predictors()) # Reescalamos
+logit_pred$pobre<-0
+logit_pred$pobre <- ifelse(logit_pred$...1 > 0.5, 1, 0)
 
+logit_pred<- logit_pred %>%
+  select(id, pobre)
 
-linear_spec <- linear_reg() %>%
-  set_engine("lm") %>%
-  set_mode("regression")
-
-
-workflow_lineal <- workflow() %>%
-  add_recipe(receta_lineal) %>%
-  add_model(linear_spec)
-
-modelo_lineal <- fit(workflow_lineal, data = train_data)
-
-
-# Sacar predicciones
-test_data <- test_data %>%
-  mutate(predicciones_lineal = predict(modelo_lineal, test_data)$.pred)
-
-
-#Logit 
-modelo_logit <- logistic_reg() %>%
-  set_engine("glm") %>%
-  set_mode("classification")
-
-workflow_logit <- workflow() %>%
-  add_recipe(receta) %>%
-  add_model(modelo_logit)
-
-modelo_logit <- workflow_logit %>%
-  fit(data = train_data)
-
-
-test_data <- test_data %>%
-  mutate(predicciones_logit = predict(modelo_logit, test_data)$.pred_class)
-matriz_logit <- conf_mat(test_data, truth = ciudad, estimate = predicciones_logit)
-print(matriz_logit)
-
-
-#Probit
-probit_spec <- logistic_reg() %>% 
-  set_engine("glm", family = stats::binomial(link = "probit")) %>%
-  set_mode("classification") %>% 
-  translate()
-
-workflow_probit <- workflow() %>%
-  add_recipe(receta) %>%
-  add_model(probit_spec)
-
-modelo_probit <- fit(workflow_probit, data = train_data)
+write.table(logit_pred, file = "Logit_class_1.csv", sep = ",", row.names = FALSE, col.names = TRUE)
 
 
 
-test_data <- test_data %>%
-  mutate(predicciones_probit = predict(modelo_probit, test_data)$.pred_class)
-matriz_probit <- conf_mat(test_data, truth = ciudad, estimate = predicciones_probit)
-print(matriz_probit)
+#Arbol
+###############
+set.seed(123)
+class_arboles <- train(rec_1, 
+                       data = train, 
+                       method = "rpart",
+                       trControl = ctrl,
+                       tuneLength = 100)
+
+
+class_arboles
+
+arbol_pred <- predict(class_arboles, newdata = test) %>% 
+  bind_cols(test) 
+
+arbol_pred<- arbol_pred %>%
+  select(id, ...1)
+
+arbol_pred$pobre<-0
+arbol_pred$pobre <- ifelse(arbol_pred$...1 > 0.5, 1, 0)
+
+arbol_pred<- arbol_pred %>%
+  select(id, pobre)
+
+write.table(arbol_pred, file = "Arbol_class_1.csv", sep = ",", row.names = FALSE, col.names = TRUE)
+
+
+####BOSQUES
+###############
+
+set.seed(123)
+
+class_bosques_1 <- train(
+  rec_1, 
+  data=train,
+  method = "ranger",
+  trControl = ctrl,
+  tuneGrid=expand.grid(
+    mtry = c(1,2,3,4,5,6,7),
+    splitrule = "gini",
+    min.node.size = c(5,10,15,30))
+)
 
 
 
+##Adaboost
 
+
+set.seed(123)
+
+class_adaboost <- train(
+  rec_1, 
+  data=train,
+  method = "AdaBoost.M1",
+  trControl = ctrl,
+  tuneGrid=expand.grid(
+    mfinal = c(300,400,500),
+    maxdepth = c(1,2,3),
+    coeflearn = c('Breiman','Freund'))
+)
